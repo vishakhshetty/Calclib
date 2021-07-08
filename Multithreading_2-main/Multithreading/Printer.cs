480d48ba330205f1a831f4e52737809ae4b13f0c
vishakhshetty <57668627+vishakhshetty@users.noreply.github.com>
2021-07-08 10:26:50 +0530

    Add files via upload

---
+++ Multithreading_2-main/Multithreading/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace multithreading
+{
+    public class Printer
+    {
+        private object lockToken = new object();
+        public void PrintNumbers()
+        {
+            lock (lockToken)
+            {
+                Console.WriteLine("Thread-> {0} started @{1} and executing PrintNumbers() method", Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToLongTimeString());
+                Console.Write("your numbers");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("{0}, ", i); Thread.Sleep(500);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Multithreading_2-main/Multithreading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Multithreading Program********\n");
+            Console.WriteLine("Main thread started. ThreadID = {0}",Thread.CurrentThread.ManagedThreadId);
+            Printer p = new Printer();
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+            for(int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+
+            }
+            Console.WriteLine("All task Queued");
+            Console.ReadLine();
+        }
+
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+
+    }
+}
